bca82594bcb2ba65b26e4486976010e34ae269cc
ultimatet <trungdang288@gmail.com>
2025-06-30 17:13:56 +1000

    vercel

--- frontend/src/components/Report.js
+++ frontend/src/components/Report.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import { Radar } from "react-chartjs-2";
 import { useLocation, useNavigate } from "react-router-dom";
 import { getBenchmarkInfo } from "../lib/benchmarks";
